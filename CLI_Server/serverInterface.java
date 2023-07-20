c2c16cd6c9ef03c74dc2adbe1a8a459e8ae50c96
Kakz-code <62792288+ConradKash@users.noreply.github.com>
2023-07-20 11:45:51 +0300

    Update on the login of the Client

--- CLI_Server/serverInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-
-public interface serverInterface extends Remote {
-    public double sum(double a, double b) throws RemoteException;
-    public double subtruct(double a, double b) throws RemoteException;
-    public double multiplica(double a, double b) throws RemoteException;
-    public double divide(double a, double b) throws RemoteException;
-    
-}
